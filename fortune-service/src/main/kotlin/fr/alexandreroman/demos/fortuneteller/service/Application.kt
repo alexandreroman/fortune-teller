ffc4d8b2de79817f650ae004da77876f7bf2b179
Alexandre Roman <aroman@pivotal.io>
2018-12-07 00:42:03 +0100

    Remove direct dependency on Eureka

--- fortune-service/src/main/kotlin/fr/alexandreroman/demos/fortuneteller/service/Application.kt
+++ fortune-service/src/main/kotlin/fr/alexandreroman/demos/fortuneteller/service/Application.kt
@@ -18,9 +18,9 @@ package fr.alexandreroman.demos.fortuneteller.service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 class Application
 
